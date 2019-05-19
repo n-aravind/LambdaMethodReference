c0be7af33683f534aafd8feba9c15fa8ff888971
Aravind <aravindnareddy@gmail.com>
2019-05-19 00:34:48 -0700

    Method Reference Assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,10 @@
-# LambdaMethodReference
-
-## Assignment
-
-- Change the System.out.println method call to be a method reference
-
-```
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List names =  Arrays.asList("Anna", "Bob", "Charlie");
+        List<String> names =  Arrays.asList("Anna", "Bob", "Charlie");
 
         System.out.println("for loop\t");
         for (String name : names) {
@@ -22,7 +15,6 @@ public class Main {
         names.forEach((name) -> System.out.println(name));
 
         System.out.println("for each using lambda method reference\t");
-        names.forEach(TODO use method reference);
+        names.forEach(System.out::println);
     }
 }
-```
